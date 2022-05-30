aadfc2f173153c811260ed339499cce8f98f3486
Bishal Karki <bishal.bishal2050@gmail.com>
2022-05-30 18:10:30 +0545

    Locators

---
+++ POM/locators/locations.py
@@ -0,0 +1,6 @@
+class locators2:
+    url = 'https://demoblaze.com'
+    login = 'login2'
+    username = '//*[@id="loginusername"]'
+    password = '//*[@id="loginpassword"]'
+    button = '//*[@id="logInModal"]/div/div/div[3]/button[2]'
